a639a319608916fc9cc194e82e7344ff81e36fbe
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-18 07:12:40 -0400

    create subject model

--- threads/models.py
+++ threads/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+from tinymce.models import HTMLField
+from django.conf import settings
 
 # Create your models here.
+class Subject(models.Model):
+
+    name = models.CharField(max_length=255)
+    description = HTMLField()
+
+    def __unicode__(self):
+        return self.name
