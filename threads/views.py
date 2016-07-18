498d239f87294c50e94bffbafa6a9b66aac3effd
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-18 19:17:21 -0400

    modify views

--- threads/views.py
+++ threads/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from threads.models import Subject
 # Create your views here.
 def forum(request):
     return render(request, 'forum/forum.html',{'subjects':Subject.objects.all()})
+
+def threads(request,subject_id):
+    subject = get_object_or_404(Subject,pk=subject_id)
+    return render(request,'forum/threads.html',{'subject':subject})
+
+
