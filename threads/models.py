03cc46f1c5853f21eb89e462d559e27b3572eb57
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-18 17:46:48 -0400

    add models

--- threads/models.py
+++ threads/models.py
@@ -12,3 +12,15 @@ class Subject(models.Model):
 
     def __unicode__(self):
         return self.name
+
+class Thread(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='threads')
+    subject = models.ForeignKey(Subject,related_name='threads')
+    created_at=models.DateTimeField(default=timezone.now)
+
+class Posts(models.Model):
+    thread = models.ForeignKey(Thread, related_name='posts')
+    comment = HTMLField(blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts')
+    created_at=models.DateTimeField(default=timezone.now)
