b46fbd1c776f1df7ac7686e765e0aab4a2e02f1a
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-18 19:32:23 -0400

    modify views

--- threads/views.py
+++ threads/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from threads.models import Subject
+from threads.models import Subject, Thread, Posts
+from django.shortcuts import redirect
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.template.context_processors import csrf
 # Create your views here.
 def forum(request):
     return render(request, 'forum/forum.html',{'subjects':Subject.objects.all()})
@@ -7,5 +12,9 @@ def forum(request):
 def threads(request,subject_id):
     subject = get_object_or_404(Subject,pk=subject_id)
     return render(request,'forum/threads.html',{'subject':subject})
+
+@login_required
+def new_thread(request, subject_id):
+    pass
 
 
