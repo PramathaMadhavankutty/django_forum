45eebf8bf3166bcde201841a06b79aacac42da3b
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-18 19:34:58 -0400

    add newthread url

--- We_Are_Social/urls.py
+++ We_Are_Social/urls.py
@@ -51,5 +51,6 @@ urlpatterns = [
 #Thread Forum
     url(r'^forum/$', forum_views.forum),
     url(r'^threads/(?P<subject_id>\d+)/$', forum_views.threads,name='threads'),
+    url(r'^new_thread/(?P<subject_id>\d+)/$',  forum_views.new_thread, name='new_thread'),
 
 ]
