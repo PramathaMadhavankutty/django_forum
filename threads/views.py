2ba07baf04e5c7d27903ada04ecb1a813b3133a1
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-18 19:02:22 -0400

    modify file

--- threads/views.py
+++ threads/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from threads.models import Subject
 # Create your views here.
+def forum(request):
+    return render(request, 'forum/forum.html',{'subjects':Subject.objects.all()})
